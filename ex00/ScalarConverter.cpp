d45d5ab8b5ed67eafa08ea121fe22997faba1345
Natália Medeiros Da Silva De Almeida <natalia@MacBookPro.home>
2026-03-14 18:32:51 +0100

    stating ex00

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,8 @@
+#include "ScalarConverter.hpp"
+
+		// ScalarConverter(/* args */);
+		// ScalarConverter(const ScalarConverter& original);
+		// ScalarConverter& operator=(const ScalarConverter& rhs);
+		// ~ScalarConverter();
+
+void ScalarConverter::convert(const std::string& input);
---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,18 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <string.h>
+#include <iostream>
+
+class ScalarConverter
+{
+	public:
+		ScalarConverter(/* args */);
+		ScalarConverter(const ScalarConverter& original);
+		ScalarConverter& operator=(const ScalarConverter& rhs);
+		~ScalarConverter();
+
+		static void convert(const std::string& input);
+};
+
+#endif
